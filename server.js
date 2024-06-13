0e4453271af82d492f0c47ecedb25979ac1027e3
RaghavOG <04raghavsingla28@gmail.com>
2024-06-13 19:45:14 +0530

    Setup SocketIO and Message and Conversation Model

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import { Server } from 'socket.io';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -7,16 +7,25 @@ import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import dbConnect from './config/dbConnect.js';
 import path from 'path';
+import http from 'http';
 
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
-dotenv.config();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ["GET", "POST"],
+  },
+});
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-
 
 // Connect to MongoDB
 dbConnect();
@@ -24,32 +33,60 @@ dbConnect();
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "client")));
 
+// Socket.io user socket map
+const userSocketMap = {}; // {userId: socketId}
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port http://127.0.0.1:${PORT}/`);
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (userId != "undefined") userSocketMap[userId] = socket.id;
+
+  // Emit the list of online users
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
+});
 
+export const getReceiverSocketId = (receiverId) => {
+  return userSocketMap[receiverId];
+};
+
+
+
+
+
+
+
+
+// API routes
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 
 
-
+// Serve the frontend
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client',  'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
-
-
-
 
 // Global error handler
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
-  
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port http://127.0.0.1:${PORT}/`);
+});
+
+export { app, io, server };
